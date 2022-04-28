9b56d16851db3fa779af6c9fb11c900a9e0f0f5e
NNSnowflake <104499421+NNSnowflake@users.noreply.github.com>
2022-04-28 12:28:29 +0200

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit 
 import pandas 
-import request
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -17,6 +17,6 @@ fruits_selected = streamlit.multiselect('Pick some fruits: ', list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = request.get('https://fruityvice.com/api/fruit/watermelon')
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 streamlit.header('Fruityvice Fruit Advice!')
 streamlit.text(fruityvice_response.json())
